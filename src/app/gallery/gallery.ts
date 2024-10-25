d4d9ec1f5d210dad4b2827a34f601f1f545021f1
Red Nguyen <rednguyen1997@gmail.com>
2024-10-25 15:34:17 -0500

    added more image

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -16,6 +16,25 @@ export const Galleries: Gallery[] = [
     {
         imgSource: "sat.jpg",
         caption:""
+    },
+    {
+        imgSource: "xe1.jpg",
+        caption:""
+    },
+    {
+        imgSource: "xe2.jpg",
+        caption:""
+    },
+    {
+        imgSource: "xe3.jpg",
+        caption:""
+    },
+    {
+        imgSource: "hoinghi.jpg",
+        caption:""
+    },
+    {
+        imgSource: "betong.jpg",
+        caption:""
     }
-
 ]
